d63455c54f287adad380a1be1b57b93c20d296b1
nanfangzai <cnzh93@163.com>
2026-01-28 22:53:59 +0800

    Remove all source files, keep only build artifacts

--- src/utils/id.js
+++
@@ -1,8 +0,0 @@
-/**
- * 生成随机 ID
- * @param {string} prefix - ID 前缀，如 'route', 'upstream', 'global_rule' 等
- * @returns {string} 生成的随机 ID
- */
-export function generateId(prefix = 'id') {
-  return prefix + '_' + Date.now().toString(36) + '_' + Math.random().toString(36).substr(2, 9)
-}
